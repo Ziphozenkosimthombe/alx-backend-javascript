d37865f09c2bb5c92c37bdce9eb959e140046297
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-08 18:17:03 +0000

    9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -17,7 +16,6 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -25,7 +23,7 @@ export class StudentHolberton {
     this._holbertonClass = holbertonClass;
   }
 
-  get fullName(){
+  get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
@@ -38,15 +36,12 @@ export class StudentHolberton {
   }
 }
 
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-
-
 const listOfStudents = [student1, student2, student3, student4, student5];
 
-export default listOfStudents
+export default listOfStudents;
