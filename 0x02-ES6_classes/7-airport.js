afdf80f4a2c6f2eed02b3640c1ebcf3be452fa78
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-08 16:39:29 +0000

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,6 +1,5 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof name !== 'string' && typeof code !== 'string') throw new Error();
     this._name = name;
     this._code = code;
   }
