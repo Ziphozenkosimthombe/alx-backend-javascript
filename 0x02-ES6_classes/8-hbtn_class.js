99c8e1e0a8a2c1cd5b00928267754afbc4814a3a
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-08 17:41:54 +0000

    8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,13 +1,13 @@
-export default class HolbertonClass{
-    constructor(size, location){
-        this._size = size;
-        this._location = location;
-    }
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    [Symbol.toPrimitive](value) {
-        if (value === 'number') {
-          return this._size;
-        }
-        return this._location;
-      }
+  [Symbol.toPrimitive](value) {
+    if (value === 'number') {
+      return this._size;
+    }
+    return this._location;
+  }
 }
