e9c3204c065fb4775ec8af6fd72a01a13791dd01
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-08 10:12:57 +0000

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const roomObject = [
+    new ClassRoom(19), new ClassRoom(20), new ClassRoom(34),
+  ];
+  return roomObject;
+}
