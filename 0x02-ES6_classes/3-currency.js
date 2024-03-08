d1733153c041b0d4de69874a667a13d224f79604
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-08 12:04:12 +0000

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,29 +4,32 @@ export default class Currency {
     this._name = name;
   }
 
+  // Getter and setter for code
   get code() {
     return this._code;
   }
 
   set code(value) {
     if (typeof value !== 'string') {
-      throw new Error('TypeError: Code must be a String');
+      throw new Error('TypeError: Code must be a string');
     }
     this._code = value;
   }
 
+  // Getter and setter for name
   get name() {
     return this._name;
   }
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new Error('TypeError: Name must be a String');
+      throw new Error('TypeError: Name must be a string');
     }
     this._name = value;
   }
 
+  // Method
   displayFullCurrency() {
-    return `${this._name} ${this._code}`;
+    return `${this._name} (${this._code})`;
   }
 }
