86c45391a011095a0d5df3155defb6ed11347eec
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-08 16:34:08 +0000

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,11 @@
-export default class Airport{
-    constructor(name, code){
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' && typeof code !== 'string') throw new Error();
+    this._name = name;
+    this._code = code;
+  }
 
-        if (typeof name !== 'string' && typeof code !== 'string') throw new Error();
-        this._name = name;
-        this._code = code;
-    }
-
-    get [Symbol.toString](){
-        return `${this._code}`;
-    } 
+  get [Symbol.toString]() {
+    return `${this._code}`;
+  }
 }
