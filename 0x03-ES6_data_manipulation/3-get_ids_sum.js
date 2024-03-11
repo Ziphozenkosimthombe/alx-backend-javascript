17a66fc9531483bda709cfe400b556b18522de7b
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-11 12:19:10 +0200

    3-get_ids_sum.js

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+/**
+ * function returns the sum of all the student ids.
+ * accept a list of students (from getListStudents) as a parameter.
+ */ 
+
+
+export default function getStudentIdsSum(students){
+    const initialValue = 0;
+    return students.reduce((acc, cv) => acc + cv.id, initialValue);
+}
