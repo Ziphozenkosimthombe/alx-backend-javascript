dd3c2d085ff3bc35b86a4ded19c0e8b6256123b5
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-11 11:28:52 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,29 +1,25 @@
 /**
- * Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade
- * It should accept a list of students (from getListStudents), a city (String), and newGrades (Array of “grade” objects) as parameters.
+ * Create a function updateStudentGradeByCity that
+ * returns an array of students for a specific city with their new grade
+ * It should accept a list of students (from getListStudents),
+ * a city (String), and newGrades (Array of “grade” objects) as parameters.
  * newGrades is an array of objects with this format
 */
 
-
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    // filter students by city
-    let studentsByCity = students.filter(student => student.location === city);
-    // map students by city and add their new grade
-    studentsByCity = studentsByCity.map(student => {
-        // find the student's grade
-        student.grade = newGrades.find(grade => grade.studentId === student.id);
-        // if the student's grade is found, add the grade to the student
-        student.grade = student.grade ? student.grade.grade : 'N/A';
-        // return the student
-        return student;
-    });
-    // return the students by city
-    return studentsByCity;
+  // filter students by city
+  let studentsByCity = students.filter((student) => student.location === city);
+  // map students by city and add their new grade
+  studentsByCity = studentsByCity.map((student) => {
+    // find the student's grade
+    const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+    // a copy of the student object
+    const updateStudent = { ...student };
+    // if the student's grade is found, add the grade to the student
+    updateStudent.grade = gradeObject ? gradeObject.grade : 'N/A';
+    // return the student
+    return updateStudent;
+  });
+  // return the students by city
+  return studentsByCity;
 }
-
-
-
-
-
-
-
