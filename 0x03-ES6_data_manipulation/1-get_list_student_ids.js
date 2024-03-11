d0535a5dc9912c54d218af4e4d7fbdd86f0a0c26
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-11 09:35:31 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,14 +3,12 @@
  * from a list of objects
  * take one argument which is an array of objects
  * array is same format as getListStudent
- */ 
+ */
 
-
-export default function getListStudentIds(array){
-    //check if array is an array
-    if (Array.isArray(array)){
-        return array.map((student) => student.id);
-    }
-    return [];
+export default function getListStudentIds(array) {
+  // check if array is an array
+  if (Array.isArray(array)) {
+    return array.map((student) => student.id);
+  }
+  return [];
 }
-
