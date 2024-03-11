f7be5c5d0bc17d1d3657afc8391dae71b93e51e9
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-11 16:14:03 +0000

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,6 @@
-export default function hasValuesFromArray(set, arr){
-    for(let arrays of arr){
-        if (!set.has(arrays)) 
-            return false;
-    }
-    return true;
+export default function hasValuesFromArray(set, arr) {
+  for (const arrays of arr) {
+    if (!set.has(arrays)) return false;
+  }
+  return true;
 }
