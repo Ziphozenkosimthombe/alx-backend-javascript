521edf3f8b4ef2c14f4d7c2bff4464bdd5e9d053
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-12 12:20:10 +0200

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -3,11 +3,12 @@
 */
 
 export default function groceriesList() {
-  const groceries = new Map();
-  groceries.set('Apple', 10);
-  groceries.set('Tomatoes', 10);
-  groceries.set('Pasta', 1);
-  groceries.set('Rice', 1);
-  groceries.set('Banana', 5);
+  const groceries = new Map([
+    ['Apple', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
   return groceries;
 }
