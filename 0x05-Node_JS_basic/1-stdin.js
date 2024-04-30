c12c572721a116fa396a870a6f3f52adeaa375f5
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-30 13:09:47 +0200

    creating the program that will be executed in the command ling using the process stdout, stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+/*
+ ==> 1-stdin.js
+ ==> will be executed by the command line
+ ==> must first display the message followed by the new line
+ ==> the users must enter their name on the new line
+ ==> program should display the message your name is: followed by the name of the user
+ ==> when the user ends the program, the program should display the message
+*/ 
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on('readable', () => {
+    const userName = process.stdin.read();
+    if (userName) {
+        process.stdout.write(`Your name is: ${userName}`);
+    }
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
