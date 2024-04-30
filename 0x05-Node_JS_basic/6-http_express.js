1d3ed767476b2650d22a0fc891852c3717d663e1
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-30 14:58:38 +0200

    writing the small server using express node framework for web application

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,22 @@
+/*
+    ==> small http server using express module
+    ==> should assign to the variable app
+    ==> listen on port 1245
+    ==> display the message on the page
+*/
+
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+const HOSTNAME = 'localhost';
+
+const message = 'Hello Holberton School!';
+
+app.get('/', (req, res) => {
+    res.send(message);
+});
+
+app.listen(PORT, HOSTNAME, () => {
+    console.log(`Server listening at http://${HOSTNAME}:${PORT}`);
+});
