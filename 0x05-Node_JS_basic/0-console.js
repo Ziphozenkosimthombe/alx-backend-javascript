e8db7f273c507575cbb157d27815d7b471e3ecdb
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-30 12:28:06 +0200

    writing that will be compile using nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/*
+// function name displayMessage,
+// take one argument message
+//will the print in STDOUT the message
+*/ 
+
+const displayMessage = (message) => console.log(message);
+
+module.exports = displayMessage;
