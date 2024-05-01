85ca866523fda9e0deac8791f0e621d780ad4503
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-01 18:36:29 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,17 +5,17 @@
  ==> the users must enter their name on the new line
  ==> program should display the message your name is: followed by the name of the user
  ==> when the user ends the program, the program should display the message
-*/ 
+*/
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-    const userName = process.stdin.read();
+  const userName = process.stdin.read();
     if (userName) {
-        process.stdout.write(`Your name is: ${userName}`);
-    }
+      process.stdout.write(`Your name is: ${userName}`);
+}
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
