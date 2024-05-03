6562b9d3df5e1c35b673511f844055c846b9dfdf
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-03 14:55:20 +0200

    update unit test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,6 @@
+const {expect} = require('chai');
 const request = require('request');
-const { expect } = require('chai');
 
-<<<<<<< HEAD
-describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
-
-  it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
-      done();
-    });
-  });
-});
-=======
 describe('GET /', () => {
     it('should return 200', (done) => {
         request('http://localhost:7865', (error, response, body) => {
@@ -28,4 +15,3 @@ describe('GET /', () => {
         });
     });
 });
->>>>>>> 1a60b02e9f04c3a4dc1e1ce8d4cc1e091c7045fb
