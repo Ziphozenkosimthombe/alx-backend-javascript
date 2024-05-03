4737fbb16822a13e31038a213b284577a6b5fa48
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-03 13:23:02 +0000

    fixing bugs

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,17 +1,14 @@
-const {expect} = require('chai');
 const request = require('request');
+const { expect } = require('chai');
 
-describe('GET /', () => {
-    it('should return 200', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
-            expect(response.statusCode).to.be.equal(200);
-            done();
-        });
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
-    it('should return Welcome to the payment system', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
-            expect(body).to.be.equal('Welcome to the payment system');
-            done();
-        });
-    });
+  });
 });
--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
-    const id = req.params.id;
-    res.send(`Payment methods for cart: ${id}`);
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
 
 app.listen(PORT, () => {
